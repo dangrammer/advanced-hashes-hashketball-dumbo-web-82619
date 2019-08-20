34258f4776e05d11aea18a02199b6b26562677d8
Dan Romans <danieljromans@gmail.com>
2019-08-20 16:29:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -299,8 +299,9 @@ def player_with_longest_name
   game_hash.each do |place, team_info|
     team_info.each do |data, details|
       if data == :players
-        details.reduce do |memo, (stat, info)|
-          if stat == :player
+        details.reduce do |memo, player_info|
+          memo = player_info[:player]
+          memo.length > player_info[:player] ? 
             
           end
         end
